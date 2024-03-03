6826633178ec2994e4d5088159ff5c615e291546
Paul Welter <pwelter@loresoft.com>
2024-03-03 07:51:56 -0600

    add JsonSerializerContext for ProblemDetails

---
+++ src/FluentRest/ProblemDetailsJsonContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace FluentRest;
+
+/// <summary>
+/// JsonSerializerContext for <see cref="ProblemDetails"/>
+/// </summary>
+/// <seealso cref="System.Text.Json.Serialization.JsonSerializerContext" />
+/// <seealso cref="System.Text.Json.Serialization.Metadata.IJsonTypeInfoResolver" />
+[JsonSerializable(typeof(ProblemDetails))]
+[JsonSerializable(typeof(JsonElement))]
+public partial class ProblemDetailsJsonContext : JsonSerializerContext;
